a57ee1d42753bd2edb099b4ed3243ca3515672a8
howeihan <fannyho0923@gmail.com>
2022-11-30 03:42:43 +0800

    Deploy Next.js to gh-pages

--- _next/static/chunks/pages/product-details/product/rightImagePage-dd9e2f8a918ba482.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1831],{40177:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/product-details/product/rightImagePage",function(){return _(45686)}])}},function(n){n.O(0,[1559,3443,6066,6160,3893,890,5686,9774,2888,179],(function(){return u=40177,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
